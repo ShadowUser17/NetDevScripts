043afa1b8092efa99cbf2b5a5e80806b034d5716
ShadowUser17 <linuxforce17@gmail.com>
2024-03-11 22:12:25 +0200

    Add get_logger() to common.py

--- common.py
+++ common.py
@@ -1,3 +1,16 @@
+import os
+import logging
+
+
+def get_logger() -> logging.Logger:
+    log_level = logging.DEBUG if os.environ.get("DEBUG_MODE", "") else logging.INFO
+    logging.basicConfig(
+        format=r'%(levelname)s [%(asctime)s]: "%(message)s"',
+        datefmt=r'%Y-%m-%d %H:%M:%S', level=log_level
+    )
+    return logging.getLogger()
+
+
 class RouterOS:
     @classmethod
     def parse_as_value(self, output: str) -> dict:
