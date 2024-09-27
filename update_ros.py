8f7cfe14f91a77daab96319e912349442a5c791e
ShadowUser17 <linuxforce17@gmail.com>
2024-09-27 17:35:16 +0300

    Update update_ros.py

--- update_ros.py
+++ update_ros.py
@@ -1,24 +1,21 @@
-import sys
 import common
-
-from config import config
 from netmiko import ConnectHandler
 
 
 logger = common.get_logger()
-try:
-    with ConnectHandler(**config) as client:
-        res = client.send_command(":put [/system/package/update/check-for-updates as-value]")
-        logger.debug(res)
-
-        data = common.RouterOS.parse_as_value(res)
-        if data["installed-version"] != data["latest-version"]:
-            res = client.send_command(":put [/system/package/update/install as-value]")
+for host in common.get_config():
+    try:
+        with ConnectHandler(**host) as client:
+            res = client.send_command(":put [/system/package/update/check-for-updates as-value]")
             logger.debug(res)
 
-        else:
-            logger.info("Status: {}".format(data["status"]))
+            data = common.RouterOS.parse_as_value(res)
+            if data["installed-version"] != data["latest-version"]:
+                res = client.send_command(":put [/system/package/update/install as-value]")
+                logger.debug(res)
 
-except Exception:
-    logger.exception(__name__)
-    sys.exit(1)
+            else:
+                logger.info("Status: {}".format(data["status"]))
+
+    except Exception:
+        logger.exception(__name__)
